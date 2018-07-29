3e959280c5496037343bdd5d3097e4dbc120662c
Baha Ganyev <ganyev7@gmail.com>
2018-07-29 16:12:38 +0600

    added controllers & Xib

---
+++ JardamBerem/JardamBerem/View Controllers/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  JardamBerem
+//
+//  Created by Baha Ganyev on 29.07.2018.
+//  Copyright © 2018 Baha Ganyev. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
